50c55f3ee21237dbdec220741408ab334d9efa66
annnnangan <anna.ymngan@gmail.com>
2025-03-29 00:44:25 +0800

    fix: 🐛 Replace React Google Map Package

--- components/custom-components/studio-page/section/location/GoogleMapView.tsx
+++ components/custom-components/studio-page/section/location/GoogleMapView.tsx
@@ -1,31 +1,25 @@
 "use client";
-import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
-
+import { AdvancedMarker, APIProvider, Map, Pin } from "@vis.gl/react-google-maps";
 import { Coordinates } from "./LocationSection";
 
 interface Props {
   coordinates: Coordinates;
 }
 
-const containerStyle = {
-  width: "100%",
-  height: "200px",
-};
-
 const GoogleMapView = ({ coordinates }: Props) => {
-  const { isLoaded } = useJsApiLoader({
-    id: "google-map-script",
-    googleMapsApiKey: process.env.NEXT_PUBLIC_MAPS_API_KEY!,
-    libraries: ["geometry", "drawing"],
-  });
-
   return (
     <>
-      {isLoaded && (
-        <GoogleMap mapContainerStyle={containerStyle} center={coordinates} zoom={20}>
-          <Marker position={coordinates} />
-        </GoogleMap>
-      )}
+      {
+        <APIProvider apiKey={process.env.NEXT_PUBLIC_MAPS_API_KEY!}>
+          <div className="w-full h-[200px]">
+            <Map defaultZoom={20} center={coordinates} mapId={process.env.NEXT_PUBLIC_MAP_ID}>
+              <AdvancedMarker position={coordinates}>
+                <Pin />
+              </AdvancedMarker>
+            </Map>
+          </div>
+        </APIProvider>
+      }
     </>
   );
 };
