cc7b06726a984cbe93c224279fb496b20c943205
annnnangan <anna.ymngan@gmail.com>
2025-03-31 01:23:31 +0800

    fix: 🐛 Remove active studio swiper

--- app/studio-owner/(group)/dashboard/@studios/page.tsx
+++ app/studio-owner/(group)/dashboard/@studios/page.tsx
@@ -1,12 +1,18 @@
-import ActiveStudioSwiper from "@/components/custom-components/studio-owner/dashboard/ActiveStudioSwiper";
+import StudioMiniCard from "@/components/custom-components/studio/StudioMiniCard";
 import { sessionUser } from "@/lib/next-auth-config/session-user";
 import { dashboardService } from "@/services/Dashboard/DashboardService";
 
 const page = async () => {
   const user = await sessionUser();
-  const studioList = (await dashboardService.getStudioOwnerActiveStudio(user?.id!)).data;
+  const result = await dashboardService.getStudioOwnerActiveStudio(user?.id as string);
 
-  return <ActiveStudioSwiper slideItems={studioList!} />;
+  return (
+    <div className="grid grid-cols-4 gap-3">
+      {result?.data?.map((item) => (
+        <StudioMiniCard key={item.slug} studio_name={item.name} studio_slug={item.slug} cover_image={item.cover_photo} rating={item.rating} />
+      ))}
+    </div>
+  );
 };
 
 export default page;
