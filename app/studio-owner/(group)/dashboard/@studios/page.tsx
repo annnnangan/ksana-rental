b90ce9b672da5b1f6e90002555c24681d526dc9e
annnnangan <anna.ymngan@gmail.com>
2025-03-31 19:27:39 +0800

    fix: 🐛 Fix studio owner dashboard studio grid

--- app/studio-owner/(group)/dashboard/@studios/page.tsx
+++ app/studio-owner/(group)/dashboard/@studios/page.tsx
@@ -7,7 +7,7 @@ const page = async () => {
   const result = await dashboardService.getStudioOwnerActiveStudio(user?.id as string);
 
   return (
-    <div className="grid grid-cols-4 gap-3">
+    <div className="grid grid-cols-1 md:grid-cols-4 gap-3">
       {result?.data?.map((item) => (
         <StudioMiniCard key={item.slug} studio_name={item.name} studio_slug={item.slug} cover_image={item.cover_photo} rating={item.rating} />
       ))}
