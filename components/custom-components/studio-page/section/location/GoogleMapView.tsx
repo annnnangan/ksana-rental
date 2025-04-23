a36194a0b141993894732870d4dfe8d8abd64457
annnnangan <anna.ymngan@gmail.com>
2025-04-23 22:36:02 +0800

    fix: 🐛 Fix google map api key

--- components/custom-components/studio-page/section/location/GoogleMapView.tsx
+++ components/custom-components/studio-page/section/location/GoogleMapView.tsx
@@ -4,24 +4,22 @@ import { Coordinates } from "./LocationSection";
 
 interface Props {
   coordinates: Coordinates;
+  MAPS_API_KEY: string;
+  MAP_ID: string;
 }
 
-const GoogleMapView = ({ coordinates }: Props) => {
-  const API_KEY = (process.env.NEXT_PUBLIC_MAPS_API_KEY as string) ?? globalThis.MAPS_API_KEY;
-  console.log("GoogleMapView", API_KEY);
+const GoogleMapView = ({ coordinates, MAPS_API_KEY, MAP_ID }: Props) => {
   return (
     <>
-      {
-        <APIProvider apiKey={API_KEY}>
-          <div className="w-full h-[200px]">
-            <Map defaultZoom={20} center={coordinates} mapId={"3006abc1f20a98b1"}>
-              <AdvancedMarker position={coordinates}>
-                <Pin />
-              </AdvancedMarker>
-            </Map>
-          </div>
-        </APIProvider>
-      }
+      <APIProvider apiKey={MAPS_API_KEY}>
+        <div className="w-full h-[200px]">
+          <Map defaultZoom={20} center={coordinates} mapId={MAP_ID}>
+            <AdvancedMarker position={coordinates}>
+              <Pin />
+            </AdvancedMarker>
+          </Map>
+        </div>
+      </APIProvider>
     </>
   );
 };
--- components/custom-components/studio-page/section/location/LocationSection.tsx
+++ components/custom-components/studio-page/section/location/LocationSection.tsx
@@ -11,39 +11,37 @@ export interface Coordinates {
 }
 
 async function getLatLngForAddress(address: string, apiKey: string) {
-  try {
-    const response = await fetch(
-      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
-        address
-      )}&key=${apiKey}`
-    );
+  const response = await fetch(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+      address
+    )}&key=${apiKey}`
+  );
 
-    if (response.ok) {
-      const data = await response.json();
-      if (data.results && data.results.length > 0) {
-        const location = data.results[0].geometry.location;
-        return {
-          lat: location.lat,
-          lng: location.lng,
-        };
-      }
+  if (response.ok) {
+    const data = await response.json();
+    if (data.results && data.results.length > 0) {
+      const location = data.results[0].geometry.location;
+      return {
+        lat: location.lat,
+        lng: location.lng,
+      };
     }
-
-    throw new Error("Unable to retrieve coordinates for the address.");
-  } catch (error) {
-    console.error("Error fetching coordinates:", error);
-    throw error;
+  } else {
+    return undefined;
   }
 }
+const MAPS_API_KEY = process.env.NEXT_PUBLIC_MAPS_API_KEY;
+const MAP_ID = process.env.NEXT_PUBLIC_MAP_ID;
 
 const LocationSection = async ({ address }: Props) => {
-  const API_KEY = (process.env.NEXT_PUBLIC_MAPS_API_KEY as string) ?? globalThis.MAPS_API_KEY;
-  const coordinates: Coordinates = await getLatLngForAddress(address, API_KEY);
+  const coordinates: Coordinates | undefined = await getLatLngForAddress(address, MAPS_API_KEY!);
 
-  console.log("LocationSection", API_KEY);
+  if (!coordinates) {
+    return null;
+  }
   return (
     <Section title={"場地位置"}>
-      <GoogleMapView coordinates={coordinates} />
+      <GoogleMapView coordinates={coordinates} MAPS_API_KEY={MAPS_API_KEY!} MAP_ID={MAP_ID!} />
     </Section>
   );
 };
